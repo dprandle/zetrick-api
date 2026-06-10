fc40ad71f16698bcb81fd4544bcd9064811e9f77
dprandle <daniel@zetrickllc.com>
2026-06-10 15:22:34 -0500

    Remove compiled dist output from version control

--- dist/bootstrap.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-globalThis.ilog = console.log;
-globalThis.dlog = console.debug;
-globalThis.wlog = console.warn;
-globalThis.elog = console.error;
-globalThis.asrt = console.assert;
-dotenv.config();
-//# sourceMappingURL=bootstrap.js.map
